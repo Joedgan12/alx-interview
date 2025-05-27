2c12b82ade8c411c4451f724070b70aafb6f5e18
Joedgan12 <joepackins@gmail.com>
2025-05-27 05:12:24 -0700

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
-
 /**
  * Wrapper function for request object that allows it
  * to work with async and await
@@ -9,16 +7,12 @@ const request = require('request');
  *                         with parsed JSON response
  *                         and rejects with the request error.
  */
-function makeRequest(url) {
+function makeRequest (url) {
+  const request = require('request');
   return new Promise((resolve, reject) => {
     request.get(url, (error, response, body) => {
-      if (error) return reject(error);
-      try {
-        const data = JSON.parse(body);
-        resolve(data);
-      } catch (err) {
-        reject(new Error('Failed to parse JSON'));
-      }
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
     });
   });
 }
@@ -29,35 +23,18 @@ function makeRequest(url) {
  * Retrieves movie character info then prints their names
  * in order of appearance in the initial response.
  */
-async function main() {
+async function main () {
   const args = process.argv;
 
-  if (args.length < 3) {
-    console.log("Usage: node script.js <MovieID>");
-    return;
-  }
+  if (args.length < 3) return;
 
-  const movieId = args[2];
-  const movieUrl = `https://swapi-api.alx-tools.com/api/films/ ${movieId}`;
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+  const movie = await makeRequest(movieUrl);
 
-  try {
-    const movie = await makeRequest(movieUrl);
-
-    if (!movie || !movie.characters || !Array.isArray(movie.characters)) {
-      console.log("Invalid movie data");
-      return;
-    }
-
-    for (const characterUrl of movie.characters) {
-      try {
-        const character = await makeRequest(characterUrl);
-        console.log(character.name);
-      } catch (err) {
-        console.error(`Error fetching character: ${err.message}`);
-      }
-    }
-  } catch (err) {
-    console.error(`Error fetching movie: ${err.message}`);
+  if (movie.characters === undefined) return;
+  for (const characterUrl of movie.characters) {
+    const character = await makeRequest(characterUrl);
+    console.log(character.name);
   }
 }
 
